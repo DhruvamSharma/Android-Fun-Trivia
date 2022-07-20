80e84b964c177e7b455e568646f9080a4ed77dae
Dhruvam <dhruvamssharma@gmail.com>
2022-07-20 21:08:51 +0530

    navigation added

--- app/src/main/java/com/dhruvam/androidfuntrivia/MainActivity.kt
+++ app/src/main/java/com/dhruvam/androidfuntrivia/MainActivity.kt
@@ -3,11 +3,22 @@ package com.dhruvam.androidfuntrivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.dhruvam.androidfuntrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = (supportFragmentManager.findFragmentById(R.id.myHostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = (supportFragmentManager.findFragmentById(R.id.myHostFragment) as NavHostFragment).navController
+        return navController.navigateUp()
     }
 }
